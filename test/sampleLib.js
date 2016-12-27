9d428c3a8ed968be214d9e6c49cf5f3cae05a6b1
Andy Wermke <andy@dev.next-step-software.com>
2016-12-27 12:08:58 +0100

    Put heap detail information into leak error message

--- test/sampleLib.js
+++ test/sampleLib.js
@@ -6,13 +6,19 @@ let nextId = 1
 let lastInstance = null
 
 function createInstance () {
-  const newInstance = {
+  const newInstance = new SampleInstance({
     id: nextId++,
     prev: lastInstance,     // <- this is going to be our memory leak
     doStuff () {
       return newInstance.id * 2
     }
-  }
+  })
   lastInstance = newInstance
   return newInstance
+}
+
+// Using constructors instead of plain objects is a good habit
+// to get more useful debugging data
+function SampleInstance (data) {
+  Object.assign(this, data)
 }
