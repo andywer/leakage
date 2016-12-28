87edc86f3ebdb251ffa9d38b5a77659743296321
Andy Wermke <a.wermke@gmail.com>
2016-12-28 11:46:21 +0100

    Use `iterate()` in test

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,20 +1,20 @@
 // Using mocha, since for memory leak testing we want the tests run serially, anyway
 
 const expect = require('chai').expect
-const leakage = require('../lib/index')
+const iterate = require('../lib/index').iterate
 
 describe('leakage', () => {
   it('throws an error when testing leaky code', () => {
     const objects = []
 
-    expect(() => leakage.iteration(100, () => {
+    expect(() => iterate(100, () => {
       const newObject = { foo: 'bar' }
       objects.push(newObject)     // <= leak
     })).to.throw(/^Heap grew on \d subsequent garbage collections[\s\S]*Iterations between GCs: ~17[\s\S]*Final GC details:/)
   })
 
   it('does not throw when testing non-leaky code', () => {
-    expect(() => leakage.iteration(100, () => {
+    expect(() => iterate(100, () => {
       const newObject = { foo: 'bar' }
     })).to.not.throw()
   })
