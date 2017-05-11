7c4e6667519ce0a5f5efa2f568a5504d243a6d01
Andy Wermke <andy@dev.next-step-software.com>
2017-05-11 15:41:11 +0200

    Fail with useful message if user confuses iterate() & iterate.async()

---
+++ test/usability.test.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+// Using mocha, since for memory leak testing we want the tests run serially, anyway
+
+require('chai').use(require('chai-as-promised'))
+
+const expect = require('chai').expect
+const { iterate } = require('../lib/index')
+
+describe('leakage', () => {
+  it('throws on iterate(<async function>)', () => {
+    expect(
+      () => iterate(async () => {})
+    ).to.throw(/Use iterate\.async\(\)/)
+  })
+
+  it('rejects iterate.async(<sync function>)', () => {
+    return expect(
+      iterate.async(() => {})
+    ).to.eventually.be.rejectedWith(/Use iterate\(\)/)
+  })
+})
